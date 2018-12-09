58210fb2aaa5201e341fc2113fbca103a1e7fd0d
Sahibdeep Nann <initializesahib@gmail.com>
2018-12-08 22:50:35 -0800

    Added Webpack and Babel

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'tachyon.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+	exclude: [/node_modules/, /test\.js$/],
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  mode: 'production'
+}
+
